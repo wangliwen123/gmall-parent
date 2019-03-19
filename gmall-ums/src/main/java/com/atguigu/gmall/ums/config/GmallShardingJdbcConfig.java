8208e0551bb60b0aa2bfa6bdc8f4dacac616bb9f
leifengyang <534096094@qq.com>
2019-03-19 18:36:55 +0800

    --ums逆向

--- gmall-ums/src/main/java/com/atguigu/gmall/ums/config/GmallShardingJdbcConfig.java
+++ gmall-ums/src/main/java/com/atguigu/gmall/ums/config/GmallShardingJdbcConfig.java
@@ -25,12 +25,9 @@ public class GmallShardingJdbcConfig {
     @Bean
     public DataSource dataSource() throws IOException, SQLException {
 
-
         //使用sharding-jdbc创建出具有主从库的数据源
-
         DataSource dataSource = MasterSlaveDataSourceFactory
                 .createDataSource(ResourceUtils.getFile("classpath:sharding.yml"));
-
 
         return  dataSource;
     }
